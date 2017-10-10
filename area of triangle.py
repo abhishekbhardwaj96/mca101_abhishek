c44f5372f0faef4376d7e50645da1e6b445c61c6
abhishekbhardwaj96 <31645862+abhishekbhardwaj96@users.noreply.github.com>
2017-10-10 19:01:13 +0530

    area of triangle
    function that is used to find the area of triangle by using length and breadth

---
+++ area of triangle.py
@@ -0,0 +1,31 @@
+def areaRectangle(length, breadth):
+    '''
+    objective: to compute the area of rectangle
+    input parameters:
+        length: length of rectanlgle
+        breadth: breadth of the rectangle
+    approach: multiply length and breadth
+    return value: area of rectangle
+    '''
+    area = length * breadth
+    return area
+    
+def main():
+    '''
+    objective: to compute the area of rectangle
+    user inputs:
+        length: length of rectanlgle
+        breadth: breadth of the rectangle
+    approach: use function areaRectangle
+    '''
+    
+    length = int(input('enter length of rectangle: '))
+    breadth = int(input('enter breadth of rectangle: '))
+    print('length of rectangle: ', length)
+    print('breadth of rectangle: ', breadth)
+    print('area of rectangle: ',areaRectangle(length, breadth))
+    print('End of output')
+    
+if __name__ == '__main__':
+    main()
+print('End of program')
